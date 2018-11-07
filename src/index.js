eeb54abd70cd347e89b19cb6f91ca86ce68b427d
zhouzy@channelsoft.com <zhouzy@channelsoft.com>
2018-11-07 18:45:18 +0800

    1.init

--- src/index.js
+++ src/index.js
@@ -4,116 +4,75 @@
  * Date   2018/9/17
  */
 import * as THREE from 'three';
+import GLTFLoader from 'three-gltf-loader';
 import OBJLoader from 'three-obj-loader';
-import GLTFloader from 'three-gltf-loader';
+import 'three-orbitcontrols';
 
-GLTFloader(THREE);
+GLTFLoader(THREE);
+OBJLoader(THREE);
 
-var container;
-
-var camera, scene, renderer,light;
-
-var mouseX = 0, mouseY = 0;
-
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
-
-var object;
+var container, stats, controls;
+var camera, scene, renderer, light;
 
 init();
-
 animate();
-
 function init() {
-
     container = document.createElement( 'div' );
-
     document.body.appendChild( container );
 
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
-
-    camera.position.set( -1.8, 2, -2.7 );
-
-    // scene
     scene = new THREE.Scene();
+    scene.scale.set(0.5,0.5,0.5);
 
-    scene.add(camera);
-    light = new THREE.HemisphereLight( 0xbbbbff, 0x444422 );
-    light.position.set( 0, 0, 0 );
-    scene.add(light);
-    // var cube = new THREE.Mesh(
-    //     new THREE.CubeGeometry( 1,1,1 ),
-    //     new THREE.MeshNormalMaterial()
-    // );
-    // scene.add( cube );
-    var material = new THREE.MeshLambertMaterial( { color:0xffffff, side: THREE.DoubleSide } );
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
+    camera.position.set(-1,-1,-1);
+    var light = new THREE.PointLight( 0xffffff, 1 );
+    camera.add( light );
 
-    // model
+    controls = new THREE.OrbitControls( camera );
 
-    var controls = new THREE.OrbitControls( camera );
-    controls.target.set( 0, - 0.2, - 0.2 );
-    controls.update();
+    var ambientLight = new THREE.AmbientLight( 0xffffff);
+    scene.add( ambientLight);
+    scene.overrideMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
 
-    function onProgress( xhr ) {
-        if ( xhr.lengthComputable ) {
-            var percentComplete = xhr.loaded / xhr.total * 100;
-            console.log( 'model ' + Math.round( percentComplete, 2 ) + '% downloaded' );
-        }
-    }
+    let spoltLight = new THREE.SpotLight(0xFFF000);
+    spoltLight.position.set(1, 1, 1);
+    spoltLight.target = scene;
 
-    function onError( xhr ) {
-        console.log("加载失败" + xhr);
-    }
+    let loader = new GLTFLoader();
+    loader.load('modal.gltf', function ( gltf ) {
+        gltf.scene.traverse(e => {
+            if(e.position){
+                console.log(JSON.stringify(e.position));
+            }
+        });
+        scene.add(gltf.scene);
 
-    var loader = new GLTFloader();
 
-    loader.load('./dist/tiki_treasure/scene.gltf', function ( obj ) {
-        // obj.scene.traverse( function ( child ) {
-        //     if ( child.isMesh ) {
-        //         child.material = material;
-        //     }
-        // } );
-        scene = obj.scene;
-    }, onProgress, onError );
+
+        camera.lookAt(scene.position);
+    }, undefined, function ( e ) {
+        console.error( e );
+    } );
 
 
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.gammaOutput = true;
     container.appendChild( renderer.domElement );
-
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    //renderer.setClearColor(0xffffff,1.0);
     window.addEventListener( 'resize', onWindowResize, false );
+    controls.update();
 }
-
 function onWindowResize() {
-
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
-
-function onDocumentMouseMove( event ) {
-    mouseX = ( event.clientX - windowHalfX ) / 2;
-    mouseY = ( event.clientY - windowHalfY ) / 2;
-}
-
 //
 function animate() {
-    //requestAnimationFrame( animate );
-    render();
-}
-
-function render() {
-    camera.position.x += ( mouseX - camera.position.x ) * .05;
-    camera.position.y += ( - mouseY - camera.position.y ) * .05;
-    camera.lookAt(0,0,0);
+    requestAnimationFrame(animate);
     renderer.render( scene, camera );
+    controls.update();
 }
-
 
